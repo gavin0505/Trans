9ec59b38d6234033a27fce1435e3442e73213505
Gavin Zhang <zxb_worky@163.com>
2023-12-30 16:39:27 +0800

    一般初始化

---
+++ trans-shorturl/src/main/java/cc/forim/trans/shorturl/ShortUrlApplication.java
@@ -0,0 +1,18 @@
+package cc.forim.trans.shorturl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Gavin Zhang
+ * @version V1.0
+ * @since 2023/12/30 16:30
+ */
+
+@SpringBootApplication
+public class ShortUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortUrlApplication.class, args);
+    }
+}
