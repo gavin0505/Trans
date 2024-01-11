5e7739f6ccda9684433ff786a62870a5a359cd2b
Gavin Zhang <zxb_worky@163.com>
2024-01-12 01:13:38 +0800

    增加url、user、msg模块配置

---
+++ trans-user/src/main/java/cc/forim/trans/user/UserApplication.java
@@ -0,0 +1,17 @@
+package cc.forim.trans.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Gavin Zhang
+ * @version V1.0
+ */
+
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
